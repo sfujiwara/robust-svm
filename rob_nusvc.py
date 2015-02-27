ce6769cdb2e58271b67a25ce3882f4ca82940bf4
shuhei <shuhei_fujiwara@mist.i.u-tokyo.ac.jp>
2015-02-27 19:19:04 +0900

    色々実験したよ

--- rob_nusvc.py
+++ rob_nusvc.py
@@ -11,6 +11,7 @@ import time
 from sklearn.metrics import pairwise_kernels
 from sklearn import svm
 import pandas as pd
+import time
 
 # Robust-nu-SVM using linear kernel
 def robust_linear_nusvm(x, y, nu, mu, w, b):
@@ -401,63 +402,66 @@ if __name__ == '__main__':
     Numerical experiments comparing the global solution by MIP
     and a local solution by DCA
     '''
-    ##### Hyper-parameters
-    ## nu, mu = 0.61, 0.1
-    nu = np.array([0.51, 0.61, 0.71])
-    mu = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
-    trial = 50
-    ##### Synthetic data set #####
-    ## obj_dc, obj_mip = [], []
-    obj_dc = np.zeros([len(nu), trial])
-    obj_mip = np.zeros([len(nu), trial])
-    ##### Loop for seed #####
-    for s in range(trial):
-        np.random.seed(s)
-        mu1 = [2,2]
-        mu2 = [1,1]
-        cov = [[1,0],[0,1]]
-        num_p = num_n = 20
-        x_p = np.random.multivariate_normal(mu1,cov,num_p)
-        x_n = np.random.multivariate_normal(mu2,cov,num_n)
-        x = np.vstack([x_p, x_n])
-        y = np.array([1.]*num_p + [-1.]*num_n)
-        num, dim = x.shape
-        ##### Names of variables #####
-        names_w = ['w%s' % i for i in range(dim)]
-        names_xi = ['xi%s' % i for i in range(num)]
-        names_u = ['u%s' % i for i in range(num)]
-        ##### Loop for hyper-parameters #####
-        for i in range(len(nu)):
-            ##### Globally solve robust-nu-SVC #####
-            res_mip = rknsvc_mip(x, y, nu[i], mu[i])
-            ## print res_mip.solution.get_objective_value()
-            w_mip = np.array(res_mip.solution.get_values(names_w))
-            b_mip = res_mip.solution.get_values('b')
-            xi_mip = np.array(res_mip.solution.get_values(names_xi))
-            eta_mip = 1 - np.array(res_mip.solution.get_values(names_u))
-            rho_mip = res_mip.solution.get_values('rho')
-            ## obj_mip.append(res_mip.solution.get_objective_value())
-            obj_mip[i, s] = res_mip.solution.get_objective_value()
-            ##### Solved by DCA #####
-            res_dc, b_dc, rho_dc, eta_dc = robust_nusvm(x, y, nu[i], mu[i], kernel='linear')
-            alf = np.array(res_dc.solution.get_values())
-            w_dc = np.dot(x.T, alf*y)
-            ## y_i (w x_i + b) >= rho - xi
-            xi_dc = rho_dc - y * (np.dot(x, w_dc) + b_dc)
-            xi_dc[xi_dc <= 0] = 0.
-            ## obj_dc.append(np.dot(eta_dc, xi_dc) / num + np.dot(w_dc, w_dc) / 2 - (nu[i]-mu[i])*rho_dc)
-            obj_dc[i, s] = np.dot(eta_dc, xi_dc) / num + np.dot(w_dc, w_dc) / 2 - (nu[i]-mu[i])*rho_dc
-    ## obj_dc = np.array(obj_dc)
-    ## obj_mip = np.array(obj_mip)
-    ##### Box plot #####
-    plt.boxplot([obj_dc[0]/obj_mip[0], obj_dc[1]/obj_mip[1],
-                 obj_dc[2]/obj_mip[2], obj_dc[2]/obj_mip[2],
-                 obj_dc[2]/obj_mip[2]])
-    plt.ylim([0.8, 1.01])
-    plt.xlabel('(nu, mu)')
-    plt.grid()
-    plt.xticks([1, 2, 3], ['(0.51, 0.10)', '(0.51, 0.10)', '(0.51, 0.10)'])
-    plt.show()
+    ## ##### Hyper-parameters
+    ## ## nu, mu = 0.61, 0.1
+    ## nu = np.array([0.41, 0.51, 0.61, 0.71, 0.81])
+    ## mu = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
+    ## trial = 200
+    ## ##### Synthetic data set #####
+    ## ## obj_dc, obj_mip = [], []
+    ## obj_dc = np.zeros([len(nu), trial])
+    ## obj_mip = np.zeros([len(nu), trial])
+    ## ##### Loop for seed #####
+    ## for s in range(trial):
+    ##     np.random.seed(s)
+    ##     mu1 = [2,2]
+    ##     mu2 = [1,1]
+    ##     cov = [[1,0],[0,1]]
+    ##     num_p = num_n = 20
+    ##     x_p = np.random.multivariate_normal(mu1,cov,num_p)
+    ##     x_n = np.random.multivariate_normal(mu2,cov,num_n)
+    ##     x = np.vstack([x_p, x_n])
+    ##     y = np.array([1.]*num_p + [-1.]*num_n)
+    ##     num, dim = x.shape
+    ##     ##### Names of variables #####
+    ##     names_w = ['w%s' % i for i in range(dim)]
+    ##     names_xi = ['xi%s' % i for i in range(num)]
+    ##     names_u = ['u%s' % i for i in range(num)]
+    ##     ##### Loop for hyper-parameters #####
+    ##     for i in range(len(nu)):
+    ##         ##### Globally solve robust-nu-SVC #####
+    ##         res_mip = rknsvc_mip(x, y, nu[i], mu[i])
+    ##         ## print res_mip.solution.get_objective_value()
+    ##         w_mip = np.array(res_mip.solution.get_values(names_w))
+    ##         b_mip = res_mip.solution.get_values('b')
+    ##         xi_mip = np.array(res_mip.solution.get_values(names_xi))
+    ##         eta_mip = 1 - np.array(res_mip.solution.get_values(names_u))
+    ##         rho_mip = res_mip.solution.get_values('rho')
+    ##         ## obj_mip.append(res_mip.solution.get_objective_value())
+    ##         obj_mip[i, s] = res_mip.solution.get_objective_value()
+    ##         ##### Solved by DCA #####
+    ##         res_dc, b_dc, rho_dc, eta_dc = robust_nusvm(x, y, nu[i], mu[i], kernel='linear')
+    ##         alf = np.array(res_dc.solution.get_values())
+    ##         w_dc = np.dot(x.T, alf*y)
+    ##         ## y_i (w x_i + b) >= rho - xi
+    ##         xi_dc = rho_dc - y * (np.dot(x, w_dc) + b_dc)
+    ##         xi_dc[xi_dc <= 0] = 0.
+    ##         ## obj_dc.append(np.dot(eta_dc, xi_dc) / num + np.dot(w_dc, w_dc) / 2 - (nu[i]-mu[i])*rho_dc)
+    ##         obj_dc[i, s] = np.dot(eta_dc, xi_dc) / num + np.dot(w_dc, w_dc) / 2 - (nu[i]-mu[i])*rho_dc
+    ## ##### Box plot #####
+    ## plt.boxplot([obj_dc[0]/obj_mip[0], obj_dc[1]/obj_mip[1],
+    ##              obj_dc[2]/obj_mip[2], obj_dc[3]/obj_mip[3],
+    ##              obj_dc[4]/obj_mip[4]])
+    ## plt.ylim([0.8, 1.01])
+    ## plt.xlabel('(nu, mu)')
+    ## plt.ylabel('obj(DCA) / obj(MIP)')
+    ## plt.grid()
+    ## plt.xticks(range(1,6), ['(0.4, 0.1)', '(0.5, 0.1)',
+    ##                        '(0.6, 0.1)', '(0.7, 0.1)', '(0.8, 0.1)'])
+    ## plt.show()
+    ## np.savetxt('obj_dc.csv', obj_dc, fmt="%.15f", delimiter=',')
+    ## np.savetxt('obj_mip.csv', obj_mip, fmt="%.15f", delimiter=',')
+
     ##### Plot hyper plane #####
     ## ## memo w1 x1 + w2 x2 + b = 0
     ## yoko = np.array([-0., 3.])
@@ -470,3 +474,58 @@ if __name__ == '__main__':
     ## plt.legend()
     ## plt.grid()
     ## plt.show()
+
+    '''
+    Numerical experiments comparing the speed of MIP and DCA
+    '''
+    nu = 0.71
+    mu = 0.1
+    trial = 10
+    mu1 = [2,2]
+    mu2 = [1,1]
+    cov = [[1,0],[0,1]]
+    time_dca = np.zeros([5,trial])
+    time_mip = np.zeros([5,trial])
+    num_cand = [10, 20, 30, 40, 50]
+    ##### Loop for the number of samples #####
+    for i in range(len(num_cand)):
+        num_p = num_n = num_cand[i]/2
+        np.random.seed(0)
+        x_p = np.random.multivariate_normal(mu1,cov,num_p)
+        x_n = np.random.multivariate_normal(mu2,cov,num_n)
+        x = np.vstack([x_p, x_n])
+        y = np.array([1.]*num_p + [-1.]*num_n)
+        num, dim = x.shape
+        for j in range(trial):
+            start_mip = time.time()
+            res_mip = rknsvc_mip(x, y, nu, mu)
+            end_mip = time.time()
+            start_dca = time.time()
+            res_dc, b_dc, rho_dc, eta_dc = robust_nusvm(x, y, nu, mu, kernel='linear')
+            end_dca = time.time()
+            time_dca[i,j] = end_dca - start_dca
+            time_mip[i,j] = end_mip - start_mip
+
+    params = {#'backend': 'ps',
+        'axes.labelsize': 24,
+        #'text.fontsize': 18,
+        #'legend.fontsize': 28,
+        'legend.fontsize': 24,
+        'xtick.labelsize': 14,
+        'ytick.labelsize': 14,
+        #'text.usetex': False,
+    }
+    plt.rcParams.update(params)
+    ## err = np.random.normal(size=100)
+    ## plt.plot(num_cand, np.mean(time_mip, axis=1), label='MIP', lw=5)
+    ## plt.plot(num_cand, np.mean(time_dca, axis=1), label='DCA', lw=5)
+    plt.errorbar(num_cand, np.mean(time_mip, axis=1), yerr=np.std(time_mip, axis=1),
+                 label='MIP', lw=5, elinewidth=3, capsize=5)
+    plt.errorbar(num_cand, np.mean(time_dca, axis=1), yerr=np.std(time_dca, axis=1),
+                 label='DCA', lw=5, elinewidth=3, capsize=5)
+    plt.xlabel('num of samples')
+    plt.ylabel('time (sec)')
+    plt.yscale('log')
+    plt.grid()
+    plt.legend(shadow=False, prop={'size': 18}, loc='upper left')
+    plt.show()
