b7dcdde31c30f3045d3c884a46bbfcdadb36a32c
shuhei <shuhei.fujiwara+github@gmail.com>
2015-11-17 02:08:50 +0900

    graph

--- results/synthetic-final/fig.py
+++ results/synthetic-final/fig.py
@@ -10,63 +10,63 @@ def flatten_hierarchical_col(col,sep = '_'):
         return col
     else:
         new_col = ''
-        for leveli,level in enumerate(col):
+        for leveli, level in enumerate(col):
             if not level == '':
                 if not leveli == 0:
                     new_col += sep
                 new_col += level
         return new_col
 
-#df.columns = df.columns.map(flattenHierarchicalCol)
+# df.columns = df.columns.map(flattenHierarchicalCol)
 
 print 'hello'
 
-## ER-SVM + DCA
-df  = pd.read_csv('dca.csv')
+# ER-SVM + DCA
+df = pd.read_csv('dca.csv')
 gb = df.groupby(['outlier_ratio', 'nu'], as_index=False)
-df = gb.aggregate({'test_accuracy': [np.mean, np.std]})
+df = gb.aggregate({'comp_time': [np.mean, np.std]})
 df.columns = df.columns.map(flatten_hierarchical_col)
 gb = df.groupby(['outlier_ratio'])
-ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['test_accuracy_mean'], dtype=int)
+ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['comp_time_mean'], dtype=int)
 df_dca = df.iloc[ind]
 
-## ER-SVM + heuristics
-df  = pd.read_csv('var.csv')
+# ER-SVM + heuristics
+df = pd.read_csv('var.csv')
 gb = df.groupby(['outlier_ratio', 'nu'], as_index=False)
-df = gb.aggregate({'test_accuracy': [np.mean, np.std]})
+df = gb.aggregate({'comp_time': [np.mean, np.std]})
 df.columns = df.columns.map(flatten_hierarchical_col)
 gb = df.groupby(['outlier_ratio'])
-ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['test_accuracy_mean'], dtype=int)
+ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['comp_time_mean'], dtype=int)
 df_var = df.iloc[ind]
 
-## Ramp Loss SVM
+# Ramp Loss SVM
 df = pd.read_csv('ramp.csv')
 gb = df.groupby(['outlier_ratio', 'C'], as_index=False)
-df = gb.aggregate({'test_accuracy': [np.mean, np.std]})
+df = gb.aggregate({'comp_time': [np.mean, np.std]})
 df.columns = df.columns.map(flatten_hierarchical_col)
 gb = df.groupby(['outlier_ratio'])
-ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['test_accuracy_mean'], dtype=int)
+ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['comp_time_mean'], dtype=int)
 df_ramp = df.iloc[ind]
 
-## Enu-SVM
+# Enu-SVM
 df = pd.read_csv('enu.csv')
 gb = df.groupby(['outlier_ratio', 'nu'], as_index=False)
-df = gb.aggregate({'test_accuracy': [np.mean, np.std]})
+df = gb.aggregate({'comp_time': [np.mean, np.std]})
 df.columns = df.columns.map(flatten_hierarchical_col)
 gb = df.groupby(['outlier_ratio'])
-ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['test_accuracy_mean'], dtype=int)
+ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['comp_time_mean'], dtype=int)
 df_enu = df.iloc[ind]
 
-## Enu-SVM
+# Enu-SVM
 df = pd.read_csv('csvm.csv')
 gb = df.groupby(['outlier_ratio', 'C'], as_index=False)
-df = gb.aggregate({'test_accuracy': [np.mean, np.std]})
+df = gb.aggregate({'comp_time': [np.mean, np.std]})
 df.columns = df.columns.map(flatten_hierarchical_col)
 gb = df.groupby(['outlier_ratio'])
-ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['test_accuracy_mean'], dtype=int)
+ind = np.array(df.groupby(['outlier_ratio']).agg(np.argmax)['comp_time_mean'], dtype=int)
 df_csvm = df.iloc[ind]
 
-## Set parameters
+# Set parameters
 plt.rcParams['axes.labelsize'] = 24
 plt.rcParams['lines.linewidth'] = 3
 plt.rcParams['legend.fontsize'] = 18
@@ -80,14 +80,15 @@ elw = 2
 cs = 3
 
 outlier_ratio = np.array([0.0, 0.02, 0.04, 0.06, 0.08, 0.1])
-plt.errorbar(outlier_ratio-0.001, df_dca['test_accuracy_mean'], yerr=df_dca['test_accuracy_std'], label='ER-SVM (DCA)', elinewidth=elw, capsize=cs, fmt='-')
-plt.errorbar(outlier_ratio-0.002, df_var['test_accuracy_mean'], yerr=df_var['test_accuracy_std'], label='ER-SVM (heuristics)', elinewidth=elw, capsize=cs, fmt='--')
-plt.errorbar(outlier_ratio+0.002, df_enu['test_accuracy_mean'], yerr=df_csvm['test_accuracy_std'], label='Enu-SVM', elinewidth=elw, capsize=cs, fmt='-.')
-plt.errorbar(outlier_ratio+0.001, df_csvm['test_accuracy_mean'], yerr=df_csvm['test_accuracy_std'], label='C-SVM', elinewidth=elw, capsize=cs, fmt=':')
-plt.errorbar(outlier_ratio, df_ramp['test_accuracy_mean'], yerr=df_ramp['test_accuracy_std'], label='Ramp', elinewidth=elw, capsize=cs, fmt='-x')
+plt.errorbar(outlier_ratio-0.001, df_dca['comp_time_mean'], yerr=df_dca['comp_time_std'], label='ER-SVM (DCA)', elinewidth=elw, capsize=cs, fmt='-')
+plt.errorbar(outlier_ratio-0.002, df_var['comp_time_mean'], yerr=df_var['comp_time_std'], label='ER-SVM (heuristics)', elinewidth=elw, capsize=cs, fmt='--')
+plt.errorbar(outlier_ratio+0.002, df_enu['comp_time_mean'], yerr=df_csvm['comp_time_std'], label='Enu-SVM', elinewidth=elw, capsize=cs, fmt='-.')
+plt.errorbar(outlier_ratio+0.001, df_csvm['comp_time_mean'], yerr=df_csvm['comp_time_std'], label='C-SVM', elinewidth=elw, capsize=cs, fmt=':')
+plt.errorbar(outlier_ratio, df_ramp['comp_time_mean'], yerr=df_ramp['comp_time_std'], label='Ramp', elinewidth=elw, capsize=cs, fmt='-x')
 plt.xlabel('Outlier Ratio')
-plt.ylabel('Test Accuracy')
+plt.ylabel('training time (sec)')
 plt.xlim([-0.01, 0.11])
-plt.legend(loc='lower left')
+plt.ylim([-0.1, 2])
+plt.legend(loc='upper right')
 plt.grid()
 plt.show()
---
+++ results/synthetic-final/fig2.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def flatten_hierarchical_col(col,sep = '_'):
+    if not type(col) is tuple:
+        return col
+    else:
+        new_col = ''
+        for leveli, level in enumerate(col):
+            if not level == '':
+                if not leveli == 0:
+                    new_col += sep
+                new_col += level
+        return new_col
+
+# df.columns = df.columns.map(flattenHierarchicalCol)
+
+
+# ER-SVM + DCA
+df_dca = pd.read_csv('dca.csv')
+gb = df_dca.groupby(['nu', 'outlier_ratio'], as_index=False)
+# gb = df_dca.groupby(['outlier_ratio'], as_index=False)
+df_dca = gb.aggregate({'comp_time': [np.mean, np.std]})
+
+# ER-SVM + heuristics
+df = pd.read_csv('var.csv')
+gb = df.groupby(['nu', 'outlier_ratio'], as_index=False)
+# gb = df.groupby(['outlier_ratio'], as_index=False)
+df_var = gb.aggregate({'comp_time': [np.mean, np.std]})
+
+# Ramp Loss SVM
+df = pd.read_csv('ramp.csv')
+gb = df.groupby(['C', 'outlier_ratio'], as_index=False)
+# gb = df.groupby(['outlier_ratio'], as_index=False)
+df_ramp = gb.aggregate({'comp_time': [np.mean, np.std]})
+
+# Enu-SVM
+df = pd.read_csv('enu.csv')
+gb = df.groupby(['nu', 'outlier_ratio'], as_index=False)
+# gb = df.groupby(['outlier_ratio'], as_index=False)
+df_enu = gb.aggregate({'comp_time': [np.mean, np.std]})
+
+# C-SVM
+df = pd.read_csv('csvm.csv')
+gb = df.groupby(['C', 'outlier_ratio'], as_index=False)
+# gb = df.groupby(['outlier_ratio'], as_index=False)
+df_csvm = gb.aggregate({'comp_time': [np.mean, np.std]})
+
+# Set parameters
+plt.rcParams['axes.labelsize'] = 24
+plt.rcParams['lines.linewidth'] = 3
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['legend.shadow'] = False
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['lines.markeredgewidth'] = 1
+plt.rcParams['lines.markersize'] = 9
+
+elw = 0
+cs = 3
+
+x = range(9)
+outlier_ratio = np.array([0.0, 0.02, 0.04, 0.06, 0.08, 0.1])
+x = outlier_ratio
+df = df_dca[df_dca['nu'] == 0.1]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='ER-SVM (nu = 0.1)', elinewidth=elw, capsize=cs, fmt='-')
+df = df_dca[df_dca['nu'] == 0.5]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='ER-SVM (nu = 0.5)', elinewidth=elw, capsize=cs, fmt='-^')
+# C-SVM
+df = df_csvm[df_csvm['C'] == 1e0]
+plt.errorbar(x, df['comp_time']['mean'], yerr=df['comp_time']['std'], label='C-SVM (C = 1)', elinewidth=elw, capsize=cs, fmt=':')
+df = df_csvm[df_csvm['C'] == 25]
+plt.errorbar(x, df['comp_time']['mean'], yerr=df['comp_time']['std'], label='C-SVM (C = 25)', elinewidth=elw, capsize=cs, fmt=':^')
+# Enu-SVM
+df = df_enu[df_enu['nu'] == 0.1]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='Enu-SVM (nu = 0.1)', elinewidth=elw, capsize=cs, fmt='-.')
+df = df_enu[df_enu['nu'] == 0.5]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='Enu-SVM (nu = 0.5)', elinewidth=elw, capsize=cs, fmt='-.^')
+# Heuristics
+df = df_var[df_var['nu'] == 0.1]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='Heuristics (nu = 0.1)', elinewidth=elw, capsize=cs, fmt='--')
+df = df_var[df_var['nu'] == 0.5]
+plt.errorbar(x, df['comp_time']['mean'],  yerr=df['comp_time']['std'],  label='Heuristics (nu = 0.5)', elinewidth=elw, capsize=cs, fmt='--^')
+# Ramp SVM
+df = df_ramp[df_ramp['C'] == 1e0]
+plt.errorbar(x, df['comp_time']['mean'], yerr=df['comp_time']['std'], label='Ramp (C = 1)', elinewidth=elw, capsize=cs, fmt='-s')
+df = df_ramp[df_ramp['C'] == 25]
+plt.errorbar(x, df['comp_time']['mean'], yerr=df['comp_time']['std'], label='Ramp (C = 25)', elinewidth=elw, capsize=cs, fmt='-s')
+# plt.xlabel('Outlier Ratio')
+plt.xlabel('Outlier Ratio')
+plt.ylabel('Training Time (sec)')
+plt.xlim([-0.005, 0.105])
+plt.ylim([-0.05, 0.3])
+plt.legend(loc='upper left')
+plt.grid()
+plt.show()
